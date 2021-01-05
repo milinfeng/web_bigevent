e2a4720e204f1320610c33047526d73e576f4820
705336971@qq.com <linfeng@.qq.com>
2021-01-05 10:48:55 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,6 @@
 $.ajaxPrefilter(function (options) {
     // 在 AJAX 请求之前，统一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net' + options.url;
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
     // 为有权限的接口设置 headers 请求头
     if (options.url.includes('/my/')) {
         options.headers = {
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,35 +1,33 @@
 $(function () {
-    //去注册
+    // 去注册
     $('#link_reg').on('click', function () {
-        //隐藏登录框
+        // 隐藏登录框
         $('.login-box').hide();
-        //显示注册框
+        // 显示注册框
         $('.reg-box').show();
     });
     $('#link_login').on('click', function () {
-        //隐藏注册框
+        // 隐藏注册框
         $('.reg-box').hide();
-        //显示登录框
+        // 显示登录框
         $('.login-box').show();
-
     });
-    //自定义pwd的校验规则
 
-    const form = layui.form
+    // 自定义校验规则
+    var form = layui.form;
+    var layer = layui.layer;
     form.verify({
-
-        pwd: [/^[\S]{6,12}$/, '密码必须6到12位, 且不能出现空格'],
-        // 通过形参拿到的是确认密码框中的内容
-        // 还需要拿到密码框中的内容
-        // 然后进行一次等于的判断
-        // 如果判断失败，则 return 一个提示消息即可
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         repwd: function (value) {
-            const pwd = $('.reg-box [name=password]').val();
-            if (pwd != value) {
-                return '两次输入密码不一致';
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败，则 return 一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致！';
             }
         }
-
     });
 
     // 注册功能，监听注册表单的提交事件
@@ -39,18 +37,37 @@ $(function () {
             username: $('#form_reg [name=username]').val(),
             password: $('#form_reg [name=password]').val()
         };
-        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+        $.post('/api/reguser', data, function (res) {
             if (res.status !== 0) {
                 // return console.log('注册失败', res.message);
                 return layer.msg(res.message);
             }
             // console.log('注册成功！');
-            layer.msg('注册成功');
+            layer.msg('注册成功，请登录！');
             // 主动触发点击事件
             $('#link_login').click();
         });
     });
 
-
+    // 登录功能
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault();
+        console.log($(this).serialize());
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！');
+                }
+                layer.msg('登录成功');
+                // 存储
+                localStorage.setItem('token', res.token);
+                // 跳转到后台主页
+                location.href = "/index.html";
+            }
+        });
+    });
 });
-
